a7c1f24471866f1d0f85ee2ef2745c14589f25ed
carlos daniel <1913110305@untels.edu.pe>
2023-04-24 16:35:26 -0500

    Se agrego las funciones Activity

--- app/src/main/java/com/example/damuc1_carlosaquino/MainActivity.java
+++ app/src/main/java/com/example/damuc1_carlosaquino/MainActivity.java
@@ -10,5 +10,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Entramos al metodo onCreate()");
+    }
+    @Override
+    protected void  onRestart(){
+        super.onRestart();
+        System.out.println("Entramos al metodo onRestart()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        System.out.println("Entramos al metodo onPause()");
+    }
+
+    @Override
+    protected void  onStart(){
+        super.onStart();
+        System.out.println("Entramos al metodo onStart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        System.out.println("Entramos al metodo onResume");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        System.out.println("Entramos al metodo onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        System.out.println("Entramos al metodo onDestroy");
     }
 }
